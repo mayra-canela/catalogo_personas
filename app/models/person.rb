6ef1d1b56b021b3558d27ff693c879efe14251fb
Mayra Canela <mayra.canela@tangosource.com>
2012-12-07 18:45:40 -0600

    CRUD Finished

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,5 +2,5 @@ class Person < ActiveRecord::Base
 
   attr_accessible :address, :name, :phone
   validates_presence_of :name , :address
-  validates_numericality_of :phone
+  validates_numericality_of :phone, :greater_than => 0
 end
